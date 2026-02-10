9cabc57751016e650e99ecfe84cd72a66a253590
CipherZemo <acharyakrishnan9@gmail.com>
2026-02-10 22:51:47 +0530

    portfolio

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -47,7 +47,7 @@ const Navigation = () => {
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
         >
-          Your Name
+          KRISHNAN ACHARYA
         </motion.a>
 
         {/* Desktop Navigation */}
